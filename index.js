141d98f20354afcf381376beeb077ab562192c82
Pratik Somwanshi <pratiksomwanshi570@gmail.com>
2024-02-10 19:15:37 +0530

    add morgan

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
+
+app.use(
+    morgan(":method :url :status :res[content-length] - :response-time ms")
+);
 
 app.get("/home", (req, res) => {
     return res.json({
